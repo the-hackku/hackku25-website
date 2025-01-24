e74fde59a4a0e02e939141bfc589762a93862238
William Whitehead <willclashtest@gmail.com>
2025-01-23 20:55:45 -0600

    redirect to profile if user tries to access register while registered

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -6,6 +6,7 @@ import { RegistrationForm } from "@/components/forms/RegistrationForm";
 import Link from "next/link";
 import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
 import { Info } from "lucide-react";
+import { toast } from "sonner";
 
 export default async function RegisterPage() {
   const session = await getServerSession(authOptions);
@@ -23,24 +24,8 @@ export default async function RegisterPage() {
 
   const participant = user?.ParticipantInfo;
 
-  // If the user has already registered, show an alert message using ShadCN's Alert component
   if (participant) {
-    return (
-      <div className="container mx-auto max-w-md p-6">
-        <Alert className="mb-4">
-          <Info className="h-5 w-5 text-blue-600" />
-          <AlertTitle>You&apos;re already registered!</AlertTitle>
-          <AlertDescription>
-            Thank you for registering for HackKU 2025. You can view your
-            application{" "}
-            <Link href="/profile" className="underline">
-              here
-            </Link>
-            .
-          </AlertDescription>
-        </Alert>
-      </div>
-    );
+    redirect("/profile");
   }
 
   // If not registered, show the registration form
